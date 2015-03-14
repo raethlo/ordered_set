b4541b7b7282aac36c9c73a2f5e38184ef2113c7
raethlo <roman.rostar@gmail.com>
2015-03-14 16:59:36 +0100

    adds toSet

--- set.erl
+++ set.erl
@@ -10,35 +10,36 @@
 -author(raethlo).
 
 %% API
--export([newSet/0, toList/1, insert/2, reverse/1, join/2]).
+-export([newSet/0, toList/1, toSet/1, insert/2, reverse/1, join/2]).
 %% -compile(export_all). %% replace with -export() later, for God's sake!
 
 
-newSet() -> {set,[]}.
+newSet() -> {set, []}.
 
-toList({set,List}) ->
+toList({set, List}) ->
   List.
 
-%% toSet(List) when is_list(List) ->
-%%   toSet_p(List,[]).
-%%
-%% toSet_p([],Acc) ->
-%%   {set,reverse(Acc)};
-%% toSet_p(List,Acc) ->
-%%   in.
+toSet(List) when is_list(List) ->
+  toSet_p(List, []).
 
+toSet_p([], Acc) ->
+  {set, Acc};
+toSet_p([H | T], Acc) when is_number(H) ->
+  toSet_p(T ,insert_p(Acc, H, []));
+toSet_p([_|T], Acc)  ->
+  toSet_p(T ,Acc).
 
 
 %% SET MANIPULATION FUNCTIONS
 
 insert({set, List}, Ele) when is_number(Ele) ->
-  {set,insert_p(List, Ele, [])}.
+  {set, insert_p(List, Ele, [])}.
 
-insert_p([],Ele,Acc) ->
+insert_p([], Ele, Acc) ->
   reverse([Ele | Acc]);
-insert_p( [H | T] , Ele, Acc) when Ele < H ->
-  join(Acc, [Ele | [H| T]] );
-insert_p( [H | T], Ele, Acc) when Ele > H ->
+insert_p([H | T], Ele, Acc) when Ele < H ->
+  join(Acc, [Ele | [H | T]]);
+insert_p([H | T], Ele, Acc) when Ele > H ->
   insert_p(T, Ele, [H | Acc]);
 insert_p([H | T], Ele, Acc) when Ele == H ->
   join(Acc, [H | T]).
@@ -46,8 +47,8 @@ insert_p([H | T], Ele, Acc) when Ele == H ->
 %% HELPERS
 
 %%
-join(List1,List2) when is_list(List1),is_list(List2) ->
-  join_p(reverse(List1),List2).
+join(List1, List2) when is_list(List1), is_list(List2) ->
+  join_p(reverse(List1), List2).
 
 join_p([], List) ->
   List;
@@ -59,9 +60,9 @@ join_p([H | T], List) ->
 
 
 reverse(List) when is_list(List) ->
-  reverse_p(List,[]).
+  reverse_p(List, []).
 
-reverse_p([],Acc) ->
+reverse_p([], Acc) ->
   Acc;
-reverse_p([H | T],Acc) ->
-  reverse_p(T,[ H | Acc ]).
+reverse_p([H | T], Acc) ->
+  reverse_p(T, [H | Acc]).
